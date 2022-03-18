5a32e42d41f12272eef2f3014f2c3a3776735af9
yunosuke <26004836+YunosukeYoshino@users.noreply.github.com>
2022-03-19 00:37:48 +0900

    microCms同期

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { client } from "../libs/client";
 
-export default function Home() {
+export default function Home({ blogs }) {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <p className="text-4xl">サンプルブログ!!</p>
+      <ul>
+        {blogs.map((blog) => (
+          <li className="mb-2" key={blog.id}>
+            <a>{blog.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blogs" });
+
+
+  return {
+    props: {
+      blogs: data.contents,
+    },
+  };
+};
